f681e228fd1395697d2f735517ebfb0d837f413e
galynary <107271972+galynary@users.noreply.github.com>
2022-09-10 00:57:20 +0300

    adaptive

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,21 +1,28 @@
 ;(() => {
-    const refs = {
-        openMenulBtn: document.querySelector('[data-menu-open]'),
-        closeMenulBtn: document.querySelector('[data-menu-close]'),
-        menu: document.querySelector('[data-menu]'),
-        body: document.querySelector('[bodyl]'),
-        menuNav: document.querySelector('[menu-nav]'),
+    const mobileMenu = document.querySelector('.js-menu-container')
+    const openMenuBtn = document.querySelector('.js-open-menu')
+    const closeMenuBtn = document.querySelector('.js-close-menu')
+
+    const toggleMenu = () => {
+        const isMenuOpen =
+            openMenuBtn.getAttribute('aria-expanded') === 'true' || false
+        openMenuBtn.setAttribute('aria-expanded', !isMenuOpen)
+        mobileMenu.classList.toggle('is-open')
+
+        const scrollLockMethod = !isMenuOpen
+            ? 'disableBodyScroll'
+            : 'enableBodyScroll'
+        bodyScrollLock[scrollLockMethod](document.body)
     }
 
-    refs.openMenu.Btn.addEventListener('click', toggleMenu)
-    refs.closeMenu.Btn.addEventListener('click', toggleMenu)
-    refs.menuNav.addEventListener('click', removeMenu)
+    openMenuBtn.addEventListener('click', toggleMenu)
+    closeMenuBtn.addEventListener('click', toggleMenu)
 
-    function toggleMenu() {
-        refs.menu.classList.toggle('is-hidden')
-        refs.body.classList.toggle('no-scroll')
-    }
-    function removeMenu() {
-        refs.menu.classList.add('is-hidden')
-    }
+    // Close the mobile menu on wider screens if the device orientation changes
+    window.matchMedia('(min-width: 768px)').addEventListener('change', (e) => {
+        if (!e.matches) return
+        mobileMenu.classList.remove('is-open')
+        openMenuBtn.setAttribute('aria-expanded', false)
+        bodyScrollLock.enableBodyScroll(document.body)
+    })
 })()
